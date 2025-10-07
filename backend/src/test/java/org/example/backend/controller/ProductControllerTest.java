b4a94e5aaf7e08f5a477c44be89dc9158dc55757
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-07 15:37:52 -0700

    add Controller test for Put product request
    # Conflicts:
    #	backend/src/test/java/org/example/backend/controller/ProductControllerTest.java

--- backend/src/test/java/org/example/backend/controller/ProductControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/ProductControllerTest.java
@@ -1,5 +1,5 @@
 package org.example.backend.controller;
-
+import static org.junit.jupiter.api.Assertions.*;
 import org.example.backend.model.Product;
 import org.example.backend.repo.ProductRepo;
 import org.junit.jupiter.api.Test;
