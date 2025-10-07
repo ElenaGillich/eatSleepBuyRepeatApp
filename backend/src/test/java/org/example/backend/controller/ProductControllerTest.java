ba569f930583b96996098abbb4515427d33b163f
sophiakopp <sophiakopp@gmx.de>
2025-10-07 14:54:58 +0200

    add controller tests for get all

---
+++ backend/src/test/java/org/example/backend/controller/ProductControllerTest.java
@@ -0,0 +1,63 @@
+package org.example.backend.controller;
+
+import org.example.backend.model.Product;
+import org.example.backend.repo.ProductRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class ProductControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ProductRepo productRepo;
+
+    @Test
+    void getAllProducts_returnListWithOneProduct_WhenCalled() throws Exception {
+        //given
+        Product product = new Product("1", "productTest");
+        productRepo.save(product);
+        //when
+        mockMvc.perform(get("/api/products"))
+                //then
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """     
+                        [
+                        {
+                        "id": "1",
+                        "name": "productTest"}
+                        ]
+                        """
+        ));
+    }
+    @Test
+    void getAllProducts_returnEmptyList_WhenNoProductAdded() throws Exception {
+        //given
+        //when
+        mockMvc.perform(get("/api/products"))
+                //then
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json(
+                        """     
+                        [
+                        ]
+                        """
+                ));
+    }
+
+    @Test
+    void addNewProduct() {
+    }
+}
