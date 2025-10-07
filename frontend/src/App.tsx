b691cf4553a51a23c2c13e3addcf7588d4cb41f2
sophiakopp <sophiakopp@gmx.de>
2025-10-07 14:17:23 +0200

    add Routing and Navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,18 @@
 import './App.css'
-import axios from 'axios';
-import {useEffect, useState} from "react";
-import type {Product} from "./model/Product.tsx";
+import {Route, Routes} from "react-router-dom";
+import AllProducts from "./AllProducts.tsx";
+import Navbar from "./Navbar.tsx";
 
 function App() {
-
-    const [products, setProducts] = useState<Product[]>([]);
-
-    function getAllProducts() {
-        axios.get('api/products').then(p => setProducts(p.data))
-            .catch(e => console.log(e))
-    }
-
-    useEffect(() => {
-        getAllProducts()
-    }, []);
 
 
     return (
         <>
-            {products?.map(p =>
-                <h1>{p.name}</h1>
-            )
-            }
+            <Navbar/>
+            <Routes>
+                <Route path={"/allProducts"} element={<AllProducts/>}/>
+            </Routes>
+
         </>
     )
 }
