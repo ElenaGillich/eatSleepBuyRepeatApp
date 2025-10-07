332429d75aa42cf7821160a08b00dd978e1b0e0d
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-07 15:41:57 -0700

    remove unused imports

--- backend/src/test/java/org/example/backend/controller/ProductControllerTest.java
+++ backend/src/test/java/org/example/backend/controller/ProductControllerTest.java
@@ -1,20 +1,23 @@
 package org.example.backend.controller;
 import static org.junit.jupiter.api.Assertions.*;
 import org.example.backend.model.Product;
+import org.example.backend.model.ProductDto;
 import org.example.backend.repo.ProductRepo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
 
 @SpringBootTest
 @AutoConfigureMockMvc
+
 class ProductControllerTest {
 
     @Autowired
@@ -22,6 +25,32 @@ class ProductControllerTest {
 
     @Autowired
     private ProductRepo productRepo;
+
+
+    @DirtiesContext
+    @Test
+    void updateProductById() throws Exception {
+        Product banana = new Product("001", "Banana");
+        productRepo.save(banana);
+        ProductDto bananaDto = new ProductDto("Chiquita Banana");
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/api/products/001")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                            {
+                              "name": "Chiquita Banana"
+                            }
+                        """)
+                )
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().json("""
+                                                                           {
+                                                                             "id": "001",
+                                                                             "name": "Chiquita Banana"
+                                                                           }
+                                                                           """));
+    }
+
 
     @DirtiesContext
     @Test
