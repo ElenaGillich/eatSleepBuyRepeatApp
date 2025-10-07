0b55d6872d9be61d34030c75e559fcda27f53a82
sophiakopp <sophiakopp@gmx.de>
2025-10-07 14:42:31 +0200

    add home link to navbar

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -3,6 +3,7 @@ import {Link} from "react-router-dom";
 export default function Navbar(){
     return (
         <>
+            <Link to={"/"}>Home</Link>
             <Link to={"/allProducts"}>All Products</Link>
         </>
     )
