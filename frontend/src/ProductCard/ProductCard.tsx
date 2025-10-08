92a9b1bcfa744d940e6bc4d946a5630481e50804
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-08 05:49:40 -0700

    add Edit and Save functionality

--- frontend/src/ProductCard/ProductCard.tsx
+++ frontend/src/ProductCard/ProductCard.tsx
@@ -1,6 +1,7 @@
 import type {Product} from "../model/Product.tsx";
 import './ProductCard.css'
 import {useState} from "react";
+import axios from "axios";
 
 type ProductCardProps = {
     product: Product
@@ -9,15 +10,22 @@ type ProductCardProps = {
 export default function ProductCard(props: Readonly<ProductCardProps>){
 
     const [isEditing, setIsEditing] = useState(false);
+    const [name, setName] = useState(props.product.name);
+
+    function updateProduct() {
+        axios.put(`/api/products/${props.product.id}`, {name: name})
+            .then((response) => setName(response.data.name))
+            .catch((e) => console.log(e))
+    }
 
     return(
             <div className={"product"}>
-                {!isEditing && <h2>{props.product.name}</h2>}
-                {isEditing && <input type={"text"} value={props.product.name}/>}
+                {!isEditing && <h2>{name}</h2>}
+                {isEditing && <input type={"text"} value={name} onChange={(e) => setName(e.target.value)}/>}
 
                 <h3>ID: {props.product.id}</h3>
                 {!isEditing && <button onClick={() =>setIsEditing(!isEditing)}> ✎Edit</button>}
-                {isEditing && <button onClick={() =>setIsEditing(!isEditing)}>Save</button>}
+                {isEditing && <button onClick={() => {updateProduct(); setIsEditing(!isEditing) }}>Save</button>}
             </div>
     )
 }
