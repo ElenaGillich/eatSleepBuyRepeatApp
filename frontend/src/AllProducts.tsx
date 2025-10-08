88d47df85a70e8ccd3f206f7b377e458106aee99
sophiakopp <sophiakopp@gmx.de>
2025-10-08 16:11:22 +0200

    add button to add new product

--- frontend/src/AllProducts.tsx
+++ frontend/src/AllProducts.tsx
@@ -2,20 +2,27 @@ import {useEffect, useState} from "react";
 import type {Product} from "./model/Product.tsx";
 import axios from "axios";
 import ProductCard from "./ProductCard/ProductCard.tsx";
+import {useNavigate} from "react-router-dom";
 
 export default function AllProducts() {
     const [products, setProducts] = useState<Product[]>([]);
+
+    const nav = useNavigate();
 
     function getAllProducts() {
         axios.get('api/products').then(p => setProducts(p.data))
             .catch(e => console.log(e))
     }
 
+    function goToNewProduct(){
+        nav("/newProduct")
+    }
     useEffect(() => {
         getAllProducts()
     }, []);
     return (
         <>
+            <button onClick={goToNewProduct}>New Product</button>
             {products?.map(p =>
                 <ProductCard key={p.id} product={p} />
             )
