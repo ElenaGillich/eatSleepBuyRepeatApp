e98aeedca2e0b94b740e542ad7f304aec3ee9b1a
Nina Bornemann <nina.bornemann@freenet.de>
2025-10-08 05:03:19 -0700

    add Edit and Save buttons for ProductCard

--- frontend/src/ProductCard/ProductCard.tsx
+++ frontend/src/ProductCard/ProductCard.tsx
@@ -1,5 +1,6 @@
 import type {Product} from "../model/Product.tsx";
 import './ProductCard.css'
+import {useState} from "react";
 
 type ProductCardProps = {
     product: Product
@@ -7,11 +8,16 @@ type ProductCardProps = {
 
 export default function ProductCard(props: Readonly<ProductCardProps>){
 
+    const [isEditing, setIsEditing] = useState(false);
 
     return(
             <div className={"product"}>
-                <h2>{props.product.name}</h2>
+                {!isEditing && <h2>{props.product.name}</h2>}
+                {isEditing && <input type={"text"} value={props.product.name}/>}
+
                 <h3>ID: {props.product.id}</h3>
+                {!isEditing && <button onClick={() =>setIsEditing(!isEditing)}> ✎Edit</button>}
+                {isEditing && <button onClick={() =>setIsEditing(!isEditing)}>Save</button>}
             </div>
     )
 }
