e5cc6b8120696ec5c42eba0c37e339f6c2f4fdc3
sophiakopp <sophiakopp@gmx.de>
2025-10-09 10:43:17 +0200

    added product dto and modified post method

--- frontend/src/NewProduct.tsx
+++ frontend/src/NewProduct.tsx
@@ -1,19 +1,17 @@
-import {useState} from "react";
-import type {Product} from "./model/Product.tsx";
+import {type FormEvent, useState} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import type {ProductDto} from "./model/ProductDto.tsx";
 
 export default function NewProduct(){
 
-    const [productName, setProductName] = useState<string>("");
-    const [product, setProduct] = useState<Product>({id: "", name: ""});
+    const [productDto, setProductDto] = useState<ProductDto>({name: ""});
 
     const nav = useNavigate();
 
-    function onAddNewProduct(){
-        setProduct({id: "1", name: productName})
-        axios.post("/api/products", product)
-            .then(response=> console.log(response))
+    function onAddNewProduct(e:FormEvent){
+        e.preventDefault();
+        axios.post("/api/products", productDto)
             .catch(e=>console.log(e));
         nav("/allProducts");
     }
@@ -22,7 +20,7 @@ export default function NewProduct(){
         <>
             <form onSubmit={onAddNewProduct}>
                 <label> Name:
-                    <input onChange={e => setProductName(e.target.value)}/>
+                    <input onChange={e => setProductDto({name: e.target.value})}/>
                 </label>
                 <button type={"submit"}>Submit</button>
             </form>
