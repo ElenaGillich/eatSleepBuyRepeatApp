0109b8e671cbae500dfb5c6e9fbdc6367399797c
sophiakopp <sophiakopp@gmx.de>
2025-10-09 10:19:40 +0200

    test for post method in product service

--- backend/src/test/java/org/example/backend/service/ProductServiceTest.java
+++ backend/src/test/java/org/example/backend/service/ProductServiceTest.java
@@ -3,6 +3,7 @@ package org.example.backend.service;
 import org.example.backend.model.Product;
 import org.example.backend.model.ProductDto;
 import org.example.backend.repo.ProductRepo;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -16,29 +17,31 @@ import static org.mockito.Mockito.mock;
 
 class ProductServiceTest {
 
-    ProductRepo repo = mock(ProductRepo.class);
-    ProductService service = new ProductService(repo);
 
     @Test
     void getAllProducts_returnListOfProducts_WhenCalled() {
         //GIVEN
+        ProductRepo mockRepo = mock(ProductRepo.class);
+        IdService mockIdService = mock(IdService.class);
+        ProductService service = new ProductService(mockIdService, mockRepo);
         Product product1 = new Product("1", "productTest1");
         Product product2 = new Product("2", "productTest2");
         List<Product> products = List.of(product1, product2);
 
         //WHEN
-        when(repo.findAll()).thenReturn(products);
+        when(mockRepo.findAll()).thenReturn(products);
         List<Product> actual = service.getAllProducts();
 
         //THEN
-        verify(repo).findAll();
+        verify(mockRepo).findAll();
         assertEquals(products, actual);
     }
 
     @Test
     void updateProductById_shouldUpdateName() {
         ProductRepo mockRepo = mock(ProductRepo.class);
-        ProductService productService = new ProductService(mockRepo);
+        IdService idService = mock(IdService.class);
+        ProductService productService = new ProductService(idService, mockRepo);
 
         Product existing = new Product("001", "Apple");
         ProductDto updated = new ProductDto("Pink Lady Apple");
@@ -58,7 +61,8 @@ class ProductServiceTest {
     @Test
     void updateProductById_shouldThrowException() {
         ProductRepo mockRepo = mock(ProductRepo.class);
-        ProductService productService = new ProductService(mockRepo);
+        IdService idService = mock(IdService.class);
+        ProductService productService = new ProductService(idService, mockRepo);
         ProductDto updated = new ProductDto("Pink Lady Apple");
 
         when(mockRepo.findById("005")).thenReturn(Optional.empty());
@@ -66,6 +70,26 @@ class ProductServiceTest {
         assertThrows(ResponseStatusException.class, () -> productService.updateProductById("005", updated));
         verify(mockRepo).findById("005");
         verifyNoMoreInteractions(mockRepo);
+    }
+    @Test
+    void addNewProduct_shouldReturnListWithProduct_WhenNewProductAdded() {
+        //GIVEN
+        ProductRepo mockRepo = mock(ProductRepo.class);
+        IdService mockIdService = mock(IdService.class);
+        ProductService service = new ProductService(mockIdService, mockRepo);
+
+        Product product = new Product("Test-Id", "productTest1");
+        ProductDto newProduct = new ProductDto( "productTest1");
+
+        //WHEN
+        when(mockIdService.generateNewUuid()).thenReturn("Test-Id");
+        when(mockRepo.save(product)).thenReturn(product);
+        Product actual = service.addNewProduct(newProduct);
+
+        //THEN
+        verify(mockRepo).save(product);
+        verify(mockIdService).generateNewUuid();
+        assertEquals(product, actual);
     }
 
 }
