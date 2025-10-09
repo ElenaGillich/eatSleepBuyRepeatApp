ace5e38df3e6765d48f4639c0e5d6b34dc5dee71
Yaroslava <yaroslava.moskalova@gmail.com>
2025-10-09 21:20:02 +0200

    added css to the navbar

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,11 +1,16 @@
-import {Link} from "react-router-dom";
+import {useNavigate} from "react-router-dom";
 
-export default function Navbar(){
+export default function Navbar() {
+    const nav = useNavigate();
+
     return (
-        <>
-            <Link to={"/"}>Home</Link>
-            <Link to={"/allProducts"}>All Products</Link>
-            <Link to={"/allGroceryLists"}>All Grocery Lists</Link>
-        </>
+
+        <header>
+            <nav className={"navbar"}>
+                <button className={"nav-button"} onClick={() => nav("/")}>Home</button>
+                <button className={"nav-button"} onClick={() => nav("/allProducts")}>All Products</button>
+                <button className={"nav-button"} onClick={() => nav("/allGroceryLists")}>All Grocery Lists</button>
+            </nav>
+        </header>
     )
 }
