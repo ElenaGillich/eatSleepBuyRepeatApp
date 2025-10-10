9e217c0dc9ed2623ee7efec6cdf0da44bce58523
Yaroslava <yaroslava.moskalova@gmail.com>
2025-10-10 15:53:17 +0200

    added changes to the Navbar

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,15 +1,14 @@
-import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export default function Navbar() {
-    const nav = useNavigate();
 
     return (
 
         <header>
             <nav className={"navbar"}>
-                <button className={"nav-button"} onClick={() => nav("/")}>Home</button>
-                <button className={"nav-button"} onClick={() => nav("/allProducts")}>All Products</button>
-                <button className={"nav-button"} onClick={() => nav("/allGroceryLists")}>All Grocery Lists</button>
+                <Link className={"nav-button"} to={"/"}>Home</Link>
+                <Link className={"nav-button"} to={"/allProducts"}>All Products</Link>
+                <Link className={"nav-button"} to={"/allGroceryLists"}>All Grocery Lists</Link>
             </nav>
         </header>
     )
