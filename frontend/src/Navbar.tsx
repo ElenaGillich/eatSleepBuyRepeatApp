5985da329fcf107a42677f0fc1d2e312d3a25759
Yaroslava <yaroslava.moskalova@gmail.com>
2025-10-10 15:36:12 +0200

    changed buttons to the Link in the Navbar

--- frontend/src/Navbar.tsx
+++ frontend/src/Navbar.tsx
@@ -1,7 +1,6 @@
-import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export default function Navbar() {
-    const nav = useNavigate();
 
     return (
 
@@ -10,9 +9,9 @@ export default function Navbar() {
                 <img src="/logo.png" alt="Logo" className="logo"/>
             </div>
             <nav className={"navbar"}>
-                <button className={"nav-button"} onClick={() => nav("/")}>Home</button>
-                <button className={"nav-button"} onClick={() => nav("/allProducts")}>All Products</button>
-                <button className={"nav-button"} onClick={() => nav("/allGroceryLists")}>All Grocery Lists</button>
+                <Link className={"nav-button"} to={"/"}>Home</Link>
+                <Link className={"nav-button"} to={"/allProducts"}>All Products</Link>
+                <Link className={"nav-button"} to={"/allGroceryLists"}>All Grocery Lists</Link>
             </nav>
         </header>
     )
