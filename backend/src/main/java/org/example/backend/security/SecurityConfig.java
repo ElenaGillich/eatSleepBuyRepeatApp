5df4c851f4a0daa13006d657957a8f9dd90e6978
ElenaGil <elenagillich@web.de>
2025-10-13 12:47:41 +0200

    add security in backend

---
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -0,0 +1,31 @@
+package org.example.backend.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(a -> a
+                        //.requestMatchers("/api/user-update").hasAuthority("ADMIN")
+                        .requestMatchers(HttpMethod.GET, "/api/products").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/api/grocery-list").authenticated()
+                        .requestMatchers("/api/products").authenticated()
+                        //.requestMatchers("/api/book").permitAll()
+                        .requestMatchers("/api/grocery-list").authenticated()
+                        .anyRequest().permitAll())
+                .oauth2Login(o -> o.defaultSuccessUrl("http://localhost:5173"));
+
+        return http.build();
+    }
+}
