ccbe4862bb55fa60cc1314f560f3b858d6f29092
ElenaGil <elenagillich@web.de>
2025-10-15 16:55:34 +0200

    add env variable APP_URL

--- backend/src/main/java/org/example/backend/security/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package org.example.backend.security;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -8,12 +9,18 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Objects;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${APP_URL}")
+    private String appURL;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(a -> a
@@ -24,8 +31,8 @@ public class SecurityConfig {
                         //.requestMatchers("/api/book").permitAll()
                         .requestMatchers("/api/grocery-list").authenticated()
                         .anyRequest().permitAll())
-                .logout(l -> l.logoutSuccessUrl("http://localhost:5173"))
-                .oauth2Login(o -> o.defaultSuccessUrl("http://localhost:5173"));
+                .logout(l -> l.logoutSuccessUrl(appURL))
+                .oauth2Login(o -> o.defaultSuccessUrl(appURL));
 
         return http.build();
     }
