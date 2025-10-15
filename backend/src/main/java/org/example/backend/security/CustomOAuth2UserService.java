0c9628c860611e7700965ef75abe5e09d17c059a
sophiakopp <sophiakopp@gmx.de>
2025-10-15 12:24:14 +0200

    modify loadUser return statement

--- backend/src/main/java/org/example/backend/security/CustomOAuth2UserService.java
+++ backend/src/main/java/org/example/backend/security/CustomOAuth2UserService.java
@@ -1,11 +1,15 @@
 package org.example.backend.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,12 +20,13 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
         AppUser appUser = appUserRepository.findById(oAuth2User.getName())
-                .orElseGet(()-> createAppUser(oAuth2User));
+                .orElseGet(() -> createAppUser(oAuth2User));
 
-        return oAuth2User;
+        return new DefaultOAuth2User(List.of(new SimpleGrantedAuthority(appUser.role())),
+                oAuth2User.getAttributes(), "id");
     }
 
-    private AppUser createAppUser(OAuth2User oAuth2User){
+    private AppUser createAppUser(OAuth2User oAuth2User) {
         AppUser newUser = AppUser.builder()
                 .id(oAuth2User.getName())
                 .userName(oAuth2User.getAttribute("login"))
